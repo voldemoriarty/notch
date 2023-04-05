d51334d2af0cf782049c1979b481c61659dbf718
Saad <mamin.bee16seecs@seecs.edu.pk>
2023-04-05 11:54:11 +0500

    Split in sections

--- notch_zeta_gamma.m
+++ notch_zeta_gamma.m
@@ -1,4 +1,4 @@
-% compare the effect of zeta & gamma on notch filters
+%% Compare the effect of zeta & gamma on notch filters
 close all;
 
 fc = 16;   % Hz
@@ -8,10 +8,11 @@ opt.FreqUnits = 'Hz';
 opt.Grid = 'on';
 opt.XLim = [5 50];
 
+
+%% The effect of zeta
+h=figure(1); set(h,'name','Effect of zeta');
 gamma = 0.5;
 zeta_arr = [0,0.05,0.1];
-
-h=figure(1); set(h,'name','Effect of zeta');
 for zeta = zeta_arr   
     n = design_notch(fc, gamma, zeta);
     bode(n, opt); hold on; 
@@ -24,6 +25,7 @@ legend(...
 );
 
 
+%% The effect of gamma
 zeta = 0.1;
 gamma_arr = [1.0,0.5,0.25];
 
